7fff828028af58a50e757f503e53c8ed2d4ec651
ABBAUMAR24 <abbaumar360@gmail.com>
2022-07-01 15:38:50 +0100

    add Array to the list

--- datatype/Array.js
+++ datatype/Array.js
@@ -22,6 +22,35 @@ let datas3 = ['jamilu','sani','musa','atiku']
 datas3.push('dan ladi')
 console.log(datas3)// --> ['jamilu','sani','musa','atiku','dan ladi']
 
+//unshift method --> to add element to the beginning of the array
+let datas4 = ['jamilu','sani','musa','atiku']
+datas4.unshift('dan ladi')
+console.log(datas4)// --> ['dan ladi','jamilu','sani','musa','atiku']
+
+//pop method --> to remove element from the end of the array
+let datas5 = ['jamilu','sani','musa','atiku']
+datas5.pop()
+console.log(datas5)// --> ['jamilu','sani','musa']
+
+//shift method --> to remove element from the beginning of the array    
+let datas6 = ['jamilu','sani','musa','atiku']
+datas6.shift()
+console.log(datas6)// --> ['sani','musa','atiku']
+
+//reverse method --> to reverse the array
+let datas7 = ['jamilu','sani','musa','atiku']
+datas7.reverse()
+console.log(datas7)// --> ['atiku','musa','sani','jamilu']
+
+// slice method --> to slice the array the first argument is the
+// index of the first element and the second argument is the index
+// of the last element from which it will eliminate
+let datas8 = ['jamilu','sani','musa','atiku']
+n2 = datas8.slice(1,3)
+console.log(n2)// --> ['sani','musa']
+
+
+
 
 
 
