24932bc2e5658cd4caa5a38480c1ae2bd0444f96
ABBAUMAR24 <abbaumar360@gmail.com>
2022-07-01 15:16:09 +0100

    add Array to the list

--- datatype/Array.js
+++ datatype/Array.js
@@ -7,9 +7,23 @@ Name[7]= 'dan ladi'
 console.log(Name.length) // --> 8
 // indexof method --> to find the index of the element
 let datas = ['jamilu','sani','musa','atiku']
-console.log(datas.indexOf (sani))// --> 1
+console.log(datas.indexOf ('sani'))// --> 1
 console.log(datas.indexOf ('musa'))// --> 2
 console.log(datas.indexOf ('atiku'))// --> 3
+
+// lastindexof method --> to find the last index of the element
+let datas2 = ['jamilu','sani','musa','atiku']
+console.log(datas.lastIndexOf ('sani'))// --> 1
+console.log(datas.lastIndexOf ('musa'))// --> 2
+console.log(datas.lastIndexOf ('atiku'))// --> 3
+
+//push method --> to add element to the end of the array
+let datas3 = ['jamilu','sani','musa','atiku']
+datas3.push('dan ladi')
+console.log(datas3)// --> ['jamilu','sani','musa','atiku','dan ladi']
+
+
+
 
 
 
