6c830587d18a33d5725a189a46ce2a196881f7ea
ABBAUMAR24 <abbaumar360@gmail.com>
2022-07-01 15:05:08 +0100

    add Array to the list

--- datatype/Array.js
+++ datatype/Array.js
@@ -5,6 +5,14 @@ console.log(Name.length)// --> 4
 Name[7]= 'dan ladi'
 
 console.log(Name.length) // --> 8
+// indexof method --> to find the index of the element
+let datas = ['jamilu','sani','musa','atiku']
+console.log(datas.indexOf (sani))// --> 1
+console.log(datas.indexOf ('musa'))// --> 2
+console.log(datas.indexOf ('atiku'))// --> 3
+
+
+
 
 
 
