59933d4e798c7a9fa437d4814b03435bad39e262
ABBAUMAR24 <abbaumar360@gmail.com>
2023-01-24 07:14:11 +0100

    tamplet letrall

--- Object.js
+++ Object.js
@@ -42,4 +42,16 @@ let lenght = prompt('enter lenght:')
 
 console.log(alert (`width = ${width},height = ${height},lenght = ${lenght}`))
 console.log(width + height + lenght)
+// still on tamplate letrall
+let str = "123";
+let n1 = +str;
+let n2 = -str;
+let n3 = -n2;
+let n4 = +"abcd";
+
+console.log('${str} : ${typeof str}'); // -> 123 : string
+console.log('${n1} : ${typeof n1}'); // -> 123 : number
+console.log('${n2} : ${typeof n2}'); // -> -123 : number
+console.log('${n3} : ${typeof n3}'); // -> 123 : number
+console.log('${n4} : ${typeof n4}'); //
 
