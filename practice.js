f5edfedb3a7c4d122eef50c887f2ae576a2fb8e0
ABBAUMAR24 <abbaumar360@gmail.com>
2023-01-26 06:48:03 +0100

    for loop

---
+++ practice.js
@@ -0,0 +1,12 @@
+// ternary operator
+
+let age = 20;
+
+age = age > 18? "adult" : "teenager";
+
+console.log(age);
+
+// ternary operator
+let number = prompt('Enter a number')
+let message = (number > 90) ? 'bingo!' : 'miss'
+alert (message)
