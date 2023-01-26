f54673b5699192ea855680e93bfe99c585d3d710
ABBAUMAR24 <abbaumar360@gmail.com>
2023-01-26 07:05:31 +0100

    loop continue.

--- practice.js
+++ practice.js
@@ -10,3 +10,25 @@ console.log(age);
 let number = prompt('Enter a number')
 let message = (number > 90) ? 'bingo!' : 'miss'
 alert (message)
+
+// for loop 
+let data = [1,2,3,4,5,6] 
+for (let i = 0; i > data.length; i++){
+    console.log(data[i])
+}
+// using while
+let i = 0;
+while (i < data.length){
+    console.log(data[i])
+    i++
+}
+
+// using for  
+for (let i = 0; i < data.length; i++){
+    console.log(data[i])
+}
+
+// using for of
+for (let i of data){
+    console.log(i)
+}
